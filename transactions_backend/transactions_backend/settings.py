77a46781219b15b2d9e9d846ec86fd0b13570ef3
jonathanlee3800 <jonathannnleee@gmail.com>
2024-03-09 22:02:46 +0800

    Transactions_backend
    init aws rds sql, init dockerfile, added to compose.yaml

--- transactions_backend/transactions_backend/settings.py
+++ transactions_backend/transactions_backend/settings.py
@@ -10,7 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,14 +80,33 @@ WSGI_APPLICATION = 'transactions_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'transactions',      # Name of your PostgreSQL database
+#         'USER': 'postgres',      # PostgreSQL username
+#         'PASSWORD': 'password',  # PostgreSQL password
+#         'HOST': 'localhost',               # Host where PostgreSQL is running (default: 'localhost')
+#         'PORT': '5432',                    # PostgreSQL port (default: '5432')
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'transactions',      # Name of your PostgreSQL database
-        'USER': 'postgres',      # PostgreSQL username
-        'PASSWORD': 'password',  # PostgreSQL password
-        'HOST': 'localhost',               # Host where PostgreSQL is running (default: 'localhost')
-        'PORT': '5432',                    # PostgreSQL port (default: '5432')
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'esd_backend',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'password',
+    #     'HOST': 'esd-database.cx0qua0uq8qg.ap-southeast-2.rds.amazonaws.com',
+    #     'PORT': '5432',
+    # }
+     'default': {
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
